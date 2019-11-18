7711ca120c51fd66a743d9daf71f304fd8f8d643
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-11-18 12:14:36 +0530

    operators demo

---
+++ Operators.py
@@ -0,0 +1,75 @@
+
+#Assignment
+'''
+    =
+'''
+
+a = 10
+b = 20
+
+# Arithmetic
+'''
+    +
+    -
+    *
+    /
+    %
+    **
+'''
+
+c = a + b
+c = a - b
+c = a * b
+c = a / b
+c = a % b
+c = a ** 2
+
+# Compound Assignment
+'''
+    +=
+    -=
+    *=
+    /=
+    %=
+    **=
+'''
+
+c **= a  # c = c + a
+
+# Comparision
+'''
+    <
+    >
+    <=
+    >=
+    ==
+    !=
+'''
+
+a = 20
+c = a != b
+
+# Logical
+'''
+    and
+    or
+    not
+
+    p   q   and     or  not(p)
+    1   1   1       1   0
+    1   0   0       1   0
+    0   1   0       1   1
+    0   0   0       0   1
+'''
+
+c = not(a == 10) and (b == 20)
+
+
+a = True
+b = True
+
+c = a + b
+
+print(c)
+
+
